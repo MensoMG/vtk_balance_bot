b97b5b31241aa58bdab1e8a20abd669dd62efcb0
Menso <m.massanino@gmail.com>
2022-07-01 23:33:43 +0300

    corrected the response, added an error handler

--- bot_core.py
+++ bot_core.py
@@ -19,8 +19,9 @@ dp = Dispatcher(bot=bot)
 logging.basicConfig(level=logging.INFO)
 
 
-async def clear_response(data):
-    return re.sub(r'\s*\n', '\n', data, re.MULTILINE)
+async def clear_response(response):
+    formatted_data = re.sub(r'^\s+|\s+$', ' ', response, re.UNICODE)
+    return re.sub(r' +', ' ', formatted_data, re.UNICODE)
 
 
 async def post_account(request):
@@ -29,8 +30,12 @@ async def post_account(request):
     async with session.post(vtk_url, data={'account': request}) as resp:
         html: str = await resp.text()
         parsed_html: bs4.element.Tag = bs(html, features='html5lib')
-        response: str = parsed_html.body.find('div', attrs={'class': 'alert alert-success'}).get_text()
-    await session.close()
+        try:
+            response: str = parsed_html.body.find('div', attrs={'class': 'alert alert-success'}).get_text()
+        except AttributeError:
+            return None
+        finally:
+            await session.close()
     return response
 
 
@@ -39,10 +44,13 @@ async def balance_handler(message: Message):
     alert_info = 'Для проверки баланса введите 10\-значный лицевой счет в формате `1123456789` \(без букв и пробелов\)'
 
     if message.text.startswith('11') and len(message.text) == 10:
-        response_info = await clear_response(await post_account(message.text))
+        response_info = await post_account(message.text)
         if response_info is None:
             response_info = 'Введен неправильный лицевой счет'
-        await bot.send_message(message.from_user.id, response_info, parse_mode=ParseMode.HTML)
+            await bot.send_message(message.from_user.id, response_info, parse_mode=ParseMode.HTML)
+        else:
+            clear_info = await clear_response(response_info)
+            await bot.send_message(message.from_user.id, clear_info, parse_mode=ParseMode.HTML)
     else:
         await bot.send_message(message.from_user.id, alert_info, parse_mode=ParseMode.MARKDOWN_V2)
 
